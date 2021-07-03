48487999c56d7a3deda5f449597bedff87b5b91c
Masked Phrogg <55362644+PhroggDev@users.noreply.github.com>
2021-07-03 11:40:29 -0400

    Add phroggdev as host part of .pythonanywhere.com in ALLOWED_HOSTS

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.getenv('SECRET')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', default='1')
 
-ALLOWED_HOSTS = ['127.0.0.1','.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1','phroggdev.pythonanywhere.com']
 
 
 # Application definition
